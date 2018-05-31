157d13afd9734fb9292e953b705a5ea2e1cb2408
lokeshrmitra <lokeshrmitra@gmail.com>
2018-05-31 17:28:13 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -11,7 +11,9 @@ router.get('/:id', (req, res)=>{
     let prodid = req.params.id;
     Product.find({prodid: prodid}).select('-_id')
     .then((prod)=>{
-        res.json(prod);
+        if(prod.length > 0)
+            res.json(prod);
+        else res.status(404).json({error: 'Invalid product ID'})
     },(err)=>{
         res.sendStatus(500);
     });
