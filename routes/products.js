456f83c3eb3a44d8887bdba02d55ba5703bcded9
lokeshrmitra <lokeshrmitra@gmail.com>
2019-03-28 13:29:04 +0000

    added we-push and its routes

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const webpush = require('web-push');
 
 //get the products module
 require('../models/Product');
 const Product = mongoose.model('product');
 require('../models/Subscription');
 const Subscription = mongoose.model('subscription');
+
+webpush.setVapidDetails(
+  'mailto:example@yourdomain.org',
+  "BB-vYEM6B3wVD1JX4hYuuGoFJsLeRGMt7G8to6ZAyQyHHTwzUAk3AekCU7r_yRSL3xDEX2T1m9KJsraElv5icfk",
+  "hKEheVRB2afntwIU1WtLnNusfEa2LI1luCwRBuYk_Jk"
+);
 
 router.post('/subs', (req,res)=>{
     const subObj = req.query.subscription;
@@ -24,7 +31,7 @@ router.post('/subs', (req,res)=>{
             },
             (err) => {
                 console.log(err.message);
-                res.json({message:"Server error: Can't register"});
+                res.json({message:"Server error: Can't register", error: err});
             }
         )
 })
@@ -37,6 +44,19 @@ router.get('/subs', (req, res)=>{
         res.sendStatus(500);
     });
 });
+
+router.post('/subs/send', (req, res)=>{
+    const email = req.query.email;
+    Subscription.findOne({email})
+    .then((sub)=>{
+        webpush.sendNotification(sub, 'Hello from the other side');
+        res.json({message:'push sent successfully', sub});
+    }, (err)=>{
+        res.json(err);
+    });
+});
+
+
 
 //to get info of a single product
 router.get('/:id', (req, res)=>{
